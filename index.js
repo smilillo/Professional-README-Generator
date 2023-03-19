56283f62055d602e9aaad840e494e10969d6fc42
smilillo <sofiemilillo@gmail.com>
2023-03-18 22:33:36 -0400

    added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the title of this project?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "What is this project about? Please provide a detailed desription.",
+    name: "Description"
+},
+{
+    type: "input",
+    message: "Table of Contents",
+    name: "Table of Contents"
+},
+{
+    type: "input",
+    message: "What steps are required to install this project?",
+    name: "Installation"
+},
+{
+    type: "input",
+    message: "Provide instructions for how to use this project.",
+    name: "Usage"
+},
+{
+    type: "input",
+    message: "Provide a link to the deplyed project webpage here.",
+    name: "Deployed Webpage"
+},
+{
+    type: "input",
+    message: "Attach any necessary screenshots here.",
+    name: "Screenshot"
+},
+{
+    type: "input",
+    message: "List the names and profiles of any project collaborators.",
+    name: "Credits"
+},
+{
+    type: "input",
+    message: "Enter the license used for this project.",
+    name: "License"
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
